0bc35493f2d955df937b45d44dd7fc0fe29cec1f
dlobosco <davidlobosco@gmail.com>
2017-02-20 19:04:38 -0500

    renamed due to submodule collision

---
+++ A1/P1/A1Part1/Assets/Resources/Controllers/BallController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();	
+	}
+	
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.Q)) {
+			Application.Quit ();
+		}
+		if (Input.GetKeyUp (KeyCode.R)) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene (0);
+		}
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Finish")) {
+			Vector3 upForce = new Vector3 (0, 10000, 0);
+			rb.AddForce (upForce);
+			rb.useGravity = false;
+		}
+	}
+
+}
---
+++ A1/P1/A1Part1/Assets/Resources/Controllers/PlaneController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour {
+	
+	void Update () {
+		float tiltMultiplier = 15;
+		float tiltHorizontal = -tiltMultiplier * Input.GetAxis ("Horizontal");
+		float tiltVertical = tiltMultiplier * Input.GetAxis ("Vertical") ;
+
+		Vector3 tiltAmount = new Vector3 (tiltVertical, 0, tiltHorizontal);
+		Quaternion rotation = Quaternion.Euler (tiltAmount);
+
+		transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 3.5F);
+				
+	}
+}
